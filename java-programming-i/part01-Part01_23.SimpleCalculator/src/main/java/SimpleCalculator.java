1ac0d0d08fb0e798b251024e24d6faae568efdf5
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2026-08-14 00:41:22 -0300

    feat(part-1/23): simple calculator exercise

---
+++ java-programming-i/part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class SimpleCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Write your program here
+        System.out.println("Give the first number:");
+        int firstNumber = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        int secondNumber = Integer.valueOf(scanner.nextLine());
+
+        int sum = firstNumber + secondNumber;
+        System.out.println(firstNumber + " + " + secondNumber + " = " + sum);
+
+        int subtraction = firstNumber - secondNumber;
+        System.out.println(firstNumber + " - " + secondNumber + " = " + subtraction);
+
+        int multiplication = firstNumber * secondNumber;
+        System.out.println(firstNumber + " * " + secondNumber + " = " + multiplication);
+
+        double division = (double) firstNumber / secondNumber;
+        System.out.println(firstNumber + " / " + secondNumber + " = " + division);
+    }
+}
